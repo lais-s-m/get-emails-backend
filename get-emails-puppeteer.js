3bbbcf9615ea81b963fcb476cc7044b34248c981
Laís Bomtempo <silveiramartinslais@gmail.com>
2025-05-19 11:06:11 -0400

    feat: updated args and executable path for Puppeteer launch

--- get-emails-puppeteer.js
+++ get-emails-puppeteer.js
@@ -1,8 +1,18 @@
 import puppeteer from 'puppeteer';
+require('dotenv').config();
 
 async function getAllEmailsFromAllTables(url) {
   const browser = await puppeteer.launch({
-    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--single-process',
+      '--no-zygote',
+    ],
+    executablePath:
+      process.env.NODE_ENV === 'production'
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
   });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle2' });
